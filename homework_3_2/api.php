6f306aebffb136fd9c4798d41132cf9b83b9c682
prior25 <36183074+prior25@users.noreply.github.com>
2023-10-14 18:18:38 +0300

    Update api.php

--- homework_3_2/api.php
+++ homework_3_2/api.php
@@ -1,1 +1,9 @@
+include 'class.db.php';
+$db = Db::getInstanсe();
 
+$name = $_GET['name'];
+$phone = $_GET['phone'];
+$email = $_GET['email'];
+
+$q = "INSERT INTO users (username, email, phone) VALUES ($name, $email, $phone);";
+$db->exec($q, [], __FILE__);
