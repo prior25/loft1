a72cffe90350df885b49f36dc2678892254d7fe3
prior25 <36183074+prior25@users.noreply.github.com>
2023-10-14 18:22:33 +0300

    Update api.php

--- homework_3_2/api.php
+++ homework_3_2/api.php
@@ -5,5 +5,8 @@ $name = $_GET['name'];
 $phone = $_GET['phone'];
 $email = $_GET['email'];
 
-$q = "INSERT INTO users (username, email, phone) VALUES ($name, $email, $phone);";
+$q = "INSERT INTO users (username, email, phone) VALUES ($name, $email, $phone);"; // не пишет
 $db->exec($q, [], __FILE__);
+
+//Если просто запустить api.php то вот этот запрос запишет - для теста проверял.
+//$q = "INSERT INTO users (username, email, phone) VALUES ('Иван', 'info@ivan.com', '999555666');";
