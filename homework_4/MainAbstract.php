0d86e30c050553809d7947e9360fb661bcb4e23d
prior25 <36183074+prior25@users.noreply.github.com>
2023-10-20 09:48:17 +0300

    Update MainAbstract.php

--- homework_4/MainAbstract.php
+++ homework_4/MainAbstract.php
@@ -1,1 +1,26 @@
+<?php
+abstract class MainAbstract implements TariffInterface
+{
+    public $distance;
+    protected $price_km;
+    protected $price_min;
+    public $trip_time;
 
+    private function __construct($user_distance, $user_time)
+    {
+        $this->distance = $user_distance;
+        $this->trip_time = $user_time;
+    }
+
+    public function calcprice () {
+
+        $price_km = $this->distance * $this->price_km;
+        $price_min = $this->trip_time * $this->price_min;
+        $result = $price_km + $price_min;
+        return $result;
+
+    }
+
+
+
+}
